72ef89854fa0a60d0d656523da6c9b2d307c06cb
radeqq007 <radeqq007@interia.pl>
2024-03-31 11:59:49 +0200

    Fix static file serving

--- cmd/static.go
+++ cmd/static.go
@@ -3,11 +3,6 @@ package main
 import "net/http"
 
 func setupStaticFileServer() {
-	styles := http.FileServer(http.Dir("../static/styles/"))
-	scripts := http.FileServer(http.Dir("../static/scripts/"))
-
-	// doesn't work
-	// TODO: fix this later
-	http.Handle("/styles/", http.StripPrefix("/styles/", styles))
-	http.Handle("/scripts/", http.StripPrefix("/scripts/", scripts))
+	http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("../static/styles"))))
+	http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("../static/scripts"))))
 }
